b6b5c23f40dc4e5eeca5f90e652d4da626faa573
MateusMiranda20 <position>
2023-04-27 04:51:30 -0300

    Primeira pate Javascript finalizada

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,48 @@
-const button = document.getElementById('button-converter')
+const button = document.getElementById("button-converter")
+const select = document.getElementById("trocaDeValores")
+
 
 const dolar = 5.2
+const euro = 5.9
 
 const conversao = () => {
-const inputValue = document.getElementById('input-valor').value
-const realBrasileiro = document.getElementById('valorBrasil')
-const dolarAmericano = document.getElementById('valorDolar')
+    const inputValue = document.getElementById('input-valor').value
+    const realBrasileiro = document.getElementById('valorBrasil')
+    const convertion = document.getElementById('valorDolar')
 
-realBrasileiro.innerHTML = inputValue
-dolarAmericano.innerHTML = inputValue / dolar
+    realBrasileiro.innerHTML = new Intl.NumberFormat("pt-BR",
+        { style: "currency", currency: "BRL" }
+    ).format(inputValue);
+
+    if (select.value === "US$ Dólar Americano") {
+        convertion.innerHTML = new Intl.NumberFormat("en-US",
+        { style: "currency", currency: "USD" }
+        ).format(inputValue / dolar);
+    }
+
+
+    if (select.value === "€ Euro") {
+        convertion.innerHTML = new Intl.NumberFormat("de-DE",
+        { style: "currency", currency: "EUR" }
+        ).format(inputValue / euro);
+    }
+}
+
+ trocaDeMoeda = () => {
+    const name = document.getElementById("trocaNome")
+    const imagens = document.getElementById("imgTroca")
+    
+    if (select.value === "US$ Dólar Americano" ) {
+        name.innerHTML = "US$ Dólar Americano"
+        imagens.src = "/img/Estados-unidos.png"
+    }
+    if (select.value === "€ Euro") {
+        name.innerHTML = "€ Euro"
+        imagens.src = "/img/Euro.png"
+    }
+
+    conversao()
 }
 
 button.addEventListener('click', conversao)
+select.addEventListener("change", trocaDeMoeda)
